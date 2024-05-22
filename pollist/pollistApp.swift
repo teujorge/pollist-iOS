82be81ba61ae759907e9a561d29e364f6f902ad2
Matheus Jorge <mrljorge@outlook.com>
2024-05-22 00:02:56 -0300

    Initial Commit

---
+++ pollist/pollistApp.swift
@@ -0,0 +1,17 @@
+//
+//  pollistApp.swift
+//  pollist
+//
+//  Created by Matheus Jorge on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct pollistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
